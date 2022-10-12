06bda84250b9c1954c62ed5537365b2fd77f973f
Igor Meireles <igor.meireles.dev@gmail.com>
2022-10-12 19:59:00 -0300

    feat: add header and form

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { PlusCircle } from 'phosphor-react';
+
+import { Header } from './components/Header';
+
+import './global.css';
+
+import styles from './App.module.css';
+
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <div className={styles.contentInput}>
+          <input type='text' placeholder='Adicione uma nova tarefa' />
+          <button type='submit'>
+            Criar
+            <PlusCircle size={20} />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
